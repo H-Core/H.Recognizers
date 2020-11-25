971a8b4bfd8adffdcae8e95af76c2f87c56bc59a
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-25 12:08:38 +1000

    Added converters from HomeCenter.

---
+++ src/libs/H.Converters.WitAiConverter/WitAiResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace H.NET.Converters
+{
+    internal sealed class WitAiResponse
+    {
+        [JsonProperty("text")]
+        public string? Text { get; set; }
+    }
+}
